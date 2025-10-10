d0cfa47544134fe6e604eae61bced503d455ff44
Mangineni Ruthu <mangineniruthu@gmail.com>
2025-10-10 17:40:25 -0400

    revert1

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+require("dotenv").config();
+const express=require('express');
+const connectDB=require('./config/db.js');
+const productRoutes =require('./routes/productRoutes');
+const cors = require("cors");
 
-require('dotenv').config(); // Load env variables locally
-
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
-.catch(err => {
-    console.error('MongoDB connection Failed', err);
-});
+connectDB();
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.use("/api/products",productRoutes);
+const PORT=process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
